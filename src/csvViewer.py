aec0f226394a82c920602fd0dda322147749b33c
Jung Gyu Yoon <yjg30737@gmail.com>
2023-10-09 15:06:41 +0900

    update

--- src/csvViewer.py
+++ src/csvViewer.py
@@ -3,22 +3,18 @@ import pandas as pd
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget
 
 
-class CSVViewer(QMainWindow):
+class CSVViewer(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle('CSV Viewer')
-        self.setGeometry(100, 100, 800, 600)
 
-        self.central_widget = QWidget(self)
-        self.setCentralWidget(self.central_widget)
-
-        layout = QVBoxLayout()
-        self.table_widget = QTableWidget()
-        layout.addWidget(self.table_widget)
-        self.central_widget.setLayout(layout)
+        lay = QVBoxLayout()
+        self.__tableWidget = QTableWidget()
+        lay.addWidget(self.__tableWidget)
+        self.setLayout(lay)
 
     def loadCSV(self, file_path):
         # Read the CSV file into a Pandas DataFrame
@@ -30,24 +26,17 @@ class CSVViewer(QMainWindow):
         num_rows, num_columns = df.shape
 
         # Set the number of rows and columns for the table
-        self.table_widget.setRowCount(num_rows)
-        self.table_widget.setColumnCount(num_columns)
+        self.__tableWidget.setRowCount(num_rows)
+        self.__tableWidget.setColumnCount(num_columns)
 
         # Populate the table with data
         for row_idx, row_data in df.iterrows():
             for col_idx, cell_value in enumerate(row_data):
                 item = QTableWidgetItem(str(cell_value))
-                self.table_widget.setItem(row_idx, col_idx, item)
+                self.__tableWidget.setItem(row_idx, col_idx, item)
 
         # Set the column names as headers
-        self.table_widget.setHorizontalHeaderLabels(column_names)
+        self.__tableWidget.setHorizontalHeaderLabels(column_names)
 
         # Resize columns to content
-        self.table_widget.resizeColumnsToContents()
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = CSVViewer()
-    window.loadCSV('./data/pima-indians-diabetes3.csv')  # Replace 'your_csv_file.csv' with your CSV file path
-    window.show()
-    sys.exit(app.exec_())
+        self.__tableWidget.resizeColumnsToContents()
